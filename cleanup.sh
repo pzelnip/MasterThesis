e65b6d3d9125a5d01986f67d99aa2f827aba8bc8
aparkin <aparkin>
2010-05-17 19:09:29 +0000

    Incorporated most of George's suggestions from his initial set of feedback from an early draft.
    Added some figures of the Gem Cutter & VFPE interfaces.

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+find . -name '*.aux' -exec rm '{}' +
+find . -name '*.bbl' -exec rm '{}' +
+find . -name '*.blg' -exec rm '{}' +
+find . -name '*.lof' -exec rm '{}' +
+find . -name '*.log' -exec rm '{}' +
+find . -name '*.lop' -exec rm '{}' +
+find . -name '*.lot' -exec rm '{}' +
+find . -name '*.pdf' -exec rm '{}' +
+find . -name '*.toc' -exec rm '{}' +
